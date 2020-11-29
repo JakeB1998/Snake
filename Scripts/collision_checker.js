9dbd6b39b048b147894ec258b4b74e9e6bebd16c
Botka.Jake <jake.m.botka@gmail.com>
2020-11-28 19:47:28 -0600

    fixed some formatting

--- Scripts/collision_checker.js
+++ Scripts/collision_checker.js
@@ -1,17 +1,8 @@
-function checkCollisionWall(x,y, xMax, yMax)
-{
-    if (x <= 0 || x >= xMax || y <= 0 || y >= yMax)
-    {
-        return true;
-    }
-
-  //  console.log("X: " + x + ", " + "Y: " + y
-    //            + "\nXmax: " + xMax + ", " + "Ymax: " + yMax);
-    return false
+function checkCollisionWall(x,y, xMax, yMax) {
+  return x <= 0 || x >= xMax || y <= 0 || y >= yMa;
 }
 
-function checkCollisionOnSelf(dirX,dirY, snake, context)
-{
+function checkCollisionOnSelf(dirX,dirY, snake, context) {
   var xHead = snake.x;
   var yHead = snake.y;
   var x = xHead + dirX;
@@ -19,8 +10,8 @@ function checkCollisionOnSelf(dirX,dirY, snake, context)
   //x *= snake.snakeSpeed;
   //y *= snake.snakeSpeed;
 
-  switch (dirX) // compensating for cordinate at leftmost corner of rectangle with direction
-  {
+  // compensating for cordinate at leftmost corner of rectangle with direction
+  switch (dirX) {
     case 1 * snake.snakeSpeed:
       x += snake.xSize;
       break;
@@ -29,9 +20,8 @@ function checkCollisionOnSelf(dirX,dirY, snake, context)
     default:
       break;
   }
-
-  switch (dirY)  // compensating for cordinate at leftmost corner of rectangle with direction
-  {
+ // compensating for cordinate at leftmost corner of rectangle with direction
+  switch (dirY)  {
     case 1 * snake.snakeSpeed:
       y += snake.ySize;
       break;
@@ -47,13 +37,11 @@ function checkCollisionOnSelf(dirX,dirY, snake, context)
  //console.log(x + "," + y + "," + xHead + "," + yHead);
  // console.log(dataI[0] + "," + dataI[1] + "," + dataI[2] 
              //   + "\n" +  headData[0]  +"," + headData[1] + "," + headData[2]);
-  if (dataI[0] === headData[0] && dataI[1] === headData[1] && dataI[2] === headData[2])
-  {
-    if (dataI[0] === 0 && dataI[1] === 0 && dataI[2] === 0)
-    {
+  if (dataI[0] === headData[0] && dataI[1] === headData[1] && dataI[2] === headData[2]) {
+    if (dataI[0] === 0 && dataI[1] === 0 && dataI[2] === 0) {
      
     }
-    else{
+    else {
 
       console.log(dataI[0] + "," + dataI[1] + "," + dataI[2] 
       + "\n" +  headData[0]  +"," + headData[1] + "," + headData[2]);
